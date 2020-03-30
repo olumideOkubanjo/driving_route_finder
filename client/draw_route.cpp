8fb23bd3423de57ee7670d2e1927eb9dc47b08cc
Olumide Okubanjo <olumide.okubanjo@gmail.com>
2020-03-29 23:03:05 -0600

    FINISHED CODE

--- client/draw_route.cpp
+++ client/draw_route.cpp
@@ -1,9 +1,30 @@
 #include "draw_route.h"
 #include "map_drawing.h"
+#define BLACK 0X0000
 
 extern shared_vars shared;
 
 void draw_route() {
   // implement this!
+
+  if(shared.num_waypoints > 0){
+    for(int i = 0; i < shared.num_waypoints -1; i++){
+
+      int32_t xPoint1 = longitude_to_x(shared.map_number, shared.waypoints[i].lon);
+      xPoint1 = xPoint1 - shared.map_coords.x;
+
+      int32_t yPoint1 = latitude_to_y(shared.map_number, shared.waypoints[i].lat);
+      yPoint1 = yPoint1 - shared.map_coords.y;
+
+      int32_t xPoint2 = longitude_to_x(shared.map_number, shared.waypoints[i+1].lon);
+      xPoint2 = xPoint2 - shared.map_coords.x;
+
+      int32_t yPoint2 = latitude_to_y(shared.map_number, shared.waypoints[i+1].lat);
+      yPoint2 = yPoint2 - shared.map_coords.y;
+
+      shared.tft->drawLine(xPoint1, yPoint1, xPoint2, yPoint2, BLACK);
+      
+    }  
+  }
   
 }
--- client/serial_handling.cpp
+++ client/serial_handling.cpp
@@ -1,6 +1,43 @@
+/**
+ * HEADER 
+**/
+
 #include "serial_handling.h"
 
 extern shared_vars shared;
+
+bool fillBuffer(String& l, unsigned long long timeout = 1000){
+  char buffer[50];
+  uint8_t bufferLen = 0;
+  
+  // buffer[bufferLen] = 0;
+
+  char in_char;
+  auto start = millis();
+  while(timeout >= (millis()-start)){
+    if(Serial.available()){
+      in_char = Serial.read();
+      buffer[bufferLen]=in_char;
+      bufferLen++;
+    }
+
+    if(in_char == '\n' || in_char=='\r'){
+      l = String(buffer);
+      return true;
+    }
+  }
+
+  return false;
+}
+
+lon_lat_32 parseWayPoint(String latLonString){
+  int32_t iLat = (latLonString.substring(0, latLonString.indexOf(" "))).toInt();
+  int32_t iLon = (latLonString.substring(latLonString.indexOf(" ")+1)).toInt();
+  lon_lat_32 point;
+  point.lat = iLat;
+  point.lon = iLon;
+  return point;
+};
 
 uint8_t get_waypoints(const lon_lat_32& start, const lon_lat_32& end) {
   // Currently this does not communicate over the serial port.
@@ -8,12 +45,46 @@ uint8_t get_waypoints(const lon_lat_32& start, const lon_lat_32& end) {
   // the server over Serial using the protocol in the assignment description.
 
   // TODO: implement the communication protocol from the assignment
+  String line;
 
-  // for now, nothing is stored
-  shared.num_waypoints = 0;
+  //Request
+  Serial.print("R ");
+  Serial.print(start.lat);
+  Serial.print(" ");
+  Serial.print(start.lon);
+  Serial.print(" ");
+  Serial.print(end.lat);
+  Serial.print(" ");
+  Serial.println(end.lon);
 
-  // 1 indicates a successful exchange, of course you should only output 1
-  // in your final solution if the exchange was indeed successful
-  // (otherwise, return 0 if the communication failed)
+  //Get number of waypoints
+  if(fillBuffer(line, 10000)){
+    if(line[0]=='N'){
+      String num = line.substring(2);
+      shared.num_waypoints = num.toInt();
+      Serial.println("A");
+    }else{
+      return 0;
+    }
+  }else{
+    return 0;
+  }
+
+  //Processs waypoint
+  for(int16_t i = 0; i < shared.num_waypoints; i++){
+    if(fillBuffer(line)){
+      if(line[0]=='W'){
+        String latLonString = line.substring(2);
+        shared.waypoints[i] = parseWayPoint(latLonString);
+        Serial.println("A");
+      }else{
+        return 0;
+      }
+    }else{
+      return 0;
+    }
+  }
+
+
   return 1;
 }
